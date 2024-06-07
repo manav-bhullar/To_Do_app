89200473e3d8e85802778ef3ff50f73b28c47fc0
NEW-BORN <manavbhullar341@gmail.com>
2024-06-07 16:18:43 +0530

    chore: Refactor app.js to improve readability and fix day of the week logic

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 //jshint esversion:6
 const express = require("express");
-const bodyPaser = require("body-parser");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 app.set('view engine', "ejs");
 
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     var today = new Date();
     var currentDay = today.getDay();
-    var day="";
+    var day = "";
 
-    if(today.getDay()=== 6 || today.getDay === 0){
-        day = "weekend";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+
+        case 1:
+            day = "Monday"
+            break;
+
+        case 2:
+            day = "Tuesday";
+            break;
+
+        case 3:
+            day = "Wedesday";
+            break;
+
+        case 4:
+            day = "Thrusday";
+            break;
+
+        case 5:
+            day = "Friday";
+            break;
+
+        case 6:
+            day = "Saturday";
+            break;
+
+        default:
+            console.log("Error: current day =  " + currentDay);
     }
-    else{
-        day = "weekday";
-    }
-    res.render("list", {kindOfDay: day})
 
     
+    res.render("list", { 
+        kindOfDay: day 
+    })
+
+
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Engine started 🚒")
 });
