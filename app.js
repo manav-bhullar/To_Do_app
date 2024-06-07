b9eaa6e6e6e095d5eaf99fb43048b5be48df8200
NEW-BORN <manavbhullar341@gmail.com>
2024-06-07 15:45:08 +0530

    chore: Add ejs dependency and update homepage to render dynamic content based on the day of the week

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const express = require("express");
 const bodyPaser = require("body-parser");
 
 const app = express();
+app.set('view engine', "ejs");
+
 
 app.get("/", function(req,res){
     var today = new Date();
     var currentDay = today.getDay();
+    var day="";
+
     if(today.getDay()=== 6 || today.getDay === 0){
-        res.send("<h1>Yay Weekend!</h1>")
+        day = "weekend";
     }
     else{
-        res.sendFile(__dirname+"/index.html")
+        day = "weekday";
     }
+    res.render("list", {kindOfDay: day})
+
+    
 });
 
 app.listen(3000,function(){
