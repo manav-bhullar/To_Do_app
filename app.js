4f034ba964f981b996e41f12d6e3d254f0f35087
NEW-BORN <manavbhullar341@gmail.com>
2024-06-07 20:21:15 +0530

    Refactor app.js to improve readability and add functionality for adding items to the list

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items=["Buy Food", "Cook Food", "Eat Food"];
+
+
 app.set('view engine', "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
     var today = new Date(); // Fixed unclosed parenthesis
@@ -13,10 +17,20 @@ app.get("/", function(req, res) {
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options); // Using toLocaleDateString
+    var day = today.toLocaleDateString("en-US", options); 
 
-    res.render("list", { kindOfDay: day }); // Send data to template
+    res.render("list", { kindOfDay: day , newListItems: items}); 
+
 });
+
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+     items.push(item);
+    console.log(item);
+    res.redirect("/");
+})
+
 
 app.listen(3000, function() {
     console.log("Engine started ");
